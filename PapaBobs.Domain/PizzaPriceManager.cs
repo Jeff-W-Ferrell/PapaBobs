4a9af25e78ca9a1254aa728f80895d7314931505
Jeff Ferrell <jwferrell78@gmail.com>
2017-12-05 12:32:04 -0800

    Pizza Website created to concatenate my skills.
    Built a website that accesses a local sql database via an entity data model to practice layering concerns, avoiding leaky abstractions, and managing user input with enums and try catches.

---
+++ PapaBobs.Domain/PizzaPriceManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PapaBobs.DTO;
+
+namespace PapaBobs.Domain
+{
+    public class PizzaPriceManager
+    {
+        public static decimal CalculateCost(DTO.OrderDTO order)
+        {
+            decimal cost = 0.0M;
+            var prices = getPizzaPrices();
+            cost += CalculateSizeCost(order, prices);
+            cost += CalcuateCrustCost(order, prices);
+            cost += CalculateToppingsCost(order, prices); 
+
+            return cost;
+        }
+
+        private static decimal CalculateToppingsCost(OrderDTO order, DTO.PizzaPriceDTO prices)
+        {
+            decimal cost = 0.0M;
+
+            cost += (order.Sausage) ? prices.SauasageCost : 0M;
+            cost += (order.Pepperoni) ? prices.PepperoniCost : 0M;
+            cost += (order.Onions) ? prices.OnionsCost : 0M;
+            cost += (order.GreenPeppers) ? prices.GreenPeppersCost : 0M;
+
+            return cost;
+        }
+
+
+        private static decimal CalcuateCrustCost(OrderDTO order, DTO.PizzaPriceDTO prices)
+        {
+            decimal cost = 0.0M;
+
+            switch (order.Crust)
+            {
+                case DTO.Enums.CrustType.Regular:
+                    cost = prices.RegularCrustCost;
+                    break;
+                case DTO.Enums.CrustType.Thin:
+                    cost = prices.ThinCrusrCost;
+                    break;
+                case DTO.Enums.CrustType.Thick:
+                    cost = prices.ThickCrustCost;
+                    break;
+                default:
+                    break;
+            }
+            return cost;
+
+        }
+
+        private static decimal CalculateSizeCost(OrderDTO order, DTO.PizzaPriceDTO prices)
+        {
+            decimal cost = 0.0M;
+
+            switch (order.Size)
+            {
+                case DTO.Enums.SizeType.Small:
+                    cost = prices.SmallSizeCost;
+                    break;
+                case DTO.Enums.SizeType.Medium:
+                    cost = prices.MediumSizeCost;
+                    break;
+                case DTO.Enums.SizeType.Large:
+                    cost = prices.LargeSizeCost;
+                    break;
+                default:
+                    break;
+            }
+            return cost;
+        }
+
+        private static DTO.PizzaPriceDTO getPizzaPrices()
+        {
+            var prices = Persistence.PizzaPriceRepository.GetPizzaPrices();
+            return prices;
+        }
+    }
+}
